ce0bbe9d38a2032c2bdb2a57785fcfb97b9d7ec8
Tianfan Wang <tianfanw@gmail.com>
2014-11-13 02:07:33 -0500

    decorated search pages

--- djangoprj/settings.py
+++ djangoprj/settings.py
@@ -108,7 +108,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+STATIC_ROOT = ''
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = ( os.path.join('static'), )
 
 LOGIN_REDIRECT_URL  = '/'
 LOGIN_URL           = '/login'
